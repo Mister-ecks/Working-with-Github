d8d09f2bb6118d877d3a05049d98ef058c6a063e
Emmanuel <omosakuemmanuell22@gmail.com>
2022-02-23 13:44:53 +0100

    Created a js file with a function that adds names to an array.

--- logic.js
+++ logic.js
@@ -1,1 +1,10 @@
 const header = document.querySelector("h1");
+
+const users = [];
+const createUser = (username) => {
+  users.push(username);
+  return users;
+};
+
+const value = createUser("Emmanuel");
+console.log(value);
